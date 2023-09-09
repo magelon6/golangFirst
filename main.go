83a62821a25b16bf24380b3db01888a55bcff7ed
magelon6 <adizovmsu@gmail.com>
2023-09-10 00:08:45 +0300

    goodbye handler

--- main.go
+++ main.go
@@ -10,9 +10,11 @@ import (
 func main() {
 	l := log.New(os.Stdout, "product-api \n", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
 	http.ListenAndServe(":9090", sm)
 }
